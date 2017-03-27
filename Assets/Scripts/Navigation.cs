b329b247bfd00196382578178c9813b847c8e15a
Wahid Shafique <wahidshafique@hotmail.com>
2017-03-26 23:45:37 -0400

    .

---
+++ Assets/Scripts/Navigation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour {
+    public void Play() {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Return() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
