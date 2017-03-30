6378a9872e60a1e0c2e08bcdbd63cff2da1a54cb
Wahid Shafique <wahidshafique@hotmail.com>
2017-03-30 00:40:49 -0400

    whacky, semi functioning

--- Assets/Scripts/DiscTouch.cs
+++ Assets/Scripts/DiscTouch.cs
@@ -7,8 +7,10 @@ public class DiscTouch : MonoBehaviour {
     //placement
     [HideInInspector]
     public bool isInteractable;
+    private bool isLegal = true;
     private bool isMouseDown = false;
     private float origZ;//just to keep it in place
+    private Renderer myRenderer;
     private Rigidbody rig;
     [SerializeField]
     Transform destHanoi1;
@@ -28,16 +30,20 @@ public class DiscTouch : MonoBehaviour {
     Vector3 refPos1;
     Vector3 refPos2;
     Vector3 refPos3;
+
+    [Tooltip("Layer to ignore for the beam")]
+    private LayerMask beamMask;
     #endregion variables
 
     void Start() {
+        myRenderer = GetComponent<Renderer>();
         isInteractable = true;
         origZ = transform.position.z;
         rig = GetComponent<Rigidbody>();
         refPos1 = GetOffsetPosition(destHanoi1, false);
         refPos2 = GetOffsetPosition(destHanoi2, false);
         refPos3 = GetOffsetPosition(destHanoi3, false);
-        MoveDisc("F", "f", "f");
+        //MoveDisc("F", "f", "f");
     }
 
     void Update() {
@@ -58,16 +64,47 @@ public class DiscTouch : MonoBehaviour {
         }
     }
 
-    public void MoveDisc(string from, string to, string spare) {
+    bool isValidMove() {
+        isLegal = true;
+        RaycastHit hit;
+        float thickness = 1f; //<-- Desired thickness here.
+        Vector3 origin = transform.position - new Vector3(0, 1, 0);
+        Vector3 direction = transform.TransformDirection(Vector3.up);
+        if (Physics.SphereCast(origin, thickness, direction, out hit)) {
+            if (hit.transform.tag == "Torus") {
+                isLegal = false;
+                print(hit.transform.gameObject.name);
+            }
+        }
+        return true;
+    }
+
+    public void MoveDisc(int destinationNum) {
+        switch (destinationNum) {
+            case 1:
+                StartCoroutine(WaitAndTravel(1.0f, refPos1));
+                break;
+            case 2:
+                StartCoroutine(WaitAndTravel(1.0f, refPos2));
+                break;
+            case 3:
+                StartCoroutine(WaitAndTravel(1.0f, refPos3));
+                break;
+            default:
+                break;
+        }
     }
 
     void OnMouseDown() {
-        bool isValidMove = true;
-        if (isInteractable && isValidMove) {
+        isValidMove();
+        if (isInteractable && isLegal) {
+            myRenderer.material.color = Color.white;
             transform.position = new Vector3(transform.position.x, transform.position.y, origZ);
             transform.rotation = Quaternion.identity;
             isMouseDown = true;
             rig.isKinematic = true;
+        } else {
+            myRenderer.material.color = Color.red;
         }
     }
 
@@ -75,6 +112,19 @@ public class DiscTouch : MonoBehaviour {
         isMouseDown = false;
         rig.isKinematic = false;
     }
+    private IEnumerator WaitAndTravel(float waitTime, Vector3 refPos) {
+        LaunchProjectile();
+        yield return new WaitForSeconds(waitTime);
+        rig.velocity = Vector3.zero;
+        refPos1 = GetOffsetPosition(destHanoi1, false);
+        refPos2 = GetOffsetPosition(destHanoi2, false);
+        refPos3 = GetOffsetPosition(destHanoi3, false);
+        Vector3 ourPos = GetOffsetPosition(this.transform, true);
+        desiredDisplacement = refPos - ourPos;
+        LaunchProjectileToDest();
+        yield return new WaitForSeconds(time);
+    }
+
     #region physics
     float CalculateYImpulse(float displacement, float time) {
         float velocity = (displacement - (0.5f * Physics.gravity.y * (time * time))) / (time);
@@ -101,18 +151,6 @@ public class DiscTouch : MonoBehaviour {
             refTransform.position = new Vector3(transform.position.x, forceForHeight, transform.position.z);
         }
         return isBottom ? refTransform.position - (new Vector3(0.0f, refTransform.localScale.y, 0.0f) * 0.5f) : refTransform.position + (new Vector3(0.0f, refTransform.localScale.y, 0.0f) * 0.5f);
-    }
-    private IEnumerator WaitAndTravel(float waitTime, Vector3 refPos) {
-        LaunchProjectile();
-        yield return new WaitForSeconds(waitTime);
-        rig.velocity = Vector3.zero;
-        refPos1 = GetOffsetPosition(destHanoi1, false);
-        refPos2 = GetOffsetPosition(destHanoi2, false);
-        refPos3 = GetOffsetPosition(destHanoi3, false);
-        Vector3 ourPos = GetOffsetPosition(this.transform, true);
-        desiredDisplacement = refPos - ourPos;
-        LaunchProjectileToDest();
-        yield return new WaitForSeconds(time);
     }
     #endregion physics
 }
--- Assets/Scripts/HanoiScript.cs
+++ Assets/Scripts/HanoiScript.cs
@@ -3,23 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HanoiScript : MonoBehaviour {
+    [SerializeField]
+    DiscTouch disc1;
+    [SerializeField]
+    DiscTouch disc2;
+    [SerializeField]
+    DiscTouch disc3;
 
-	void Start () {
-        //Towers(2, "f", "t", "s");
-        StartCoroutine(MakeMove(3, "f", "t", "s"));
+    private Stack<DiscTouch> pole1;
+    private Stack<DiscTouch> pole2;
+    private Stack<DiscTouch> pole3;
+
+    void Start () {
+
+        pole1 = new Stack<DiscTouch>();
+        pole2 = new Stack<DiscTouch>();
+        pole3 = new Stack<DiscTouch>();
+
+        pole1.Push(disc3);
+        pole1.Push(disc2);
+        pole1.Push(disc1);
+
+        print("top is : " + pole1.Peek());
+        StartCoroutine(MakeMove(3, pole1, 1, pole2, 2, pole3, 3));
 	}
 
     void Update() {
 
     }
-    private IEnumerator MakeMove(int n, string fr, string to, string spare) {
+
+    private IEnumerator MakeMove(int n, Stack<DiscTouch> fr, int frInt, Stack<DiscTouch> to, int toInt, Stack<DiscTouch> spare, int spInt) {
         if (n == 1) {
-            PrintMove(fr, to);
+            yield return new WaitForSeconds(1.0f);
+            //PrintMove(fr, to);
+            DiscTouch tempfrom = fr.Peek();
+            fr.Pop();
+            tempfrom.MoveDisc(toInt);
+            to.Push(tempfrom);
             yield return new WaitForSeconds(1.0f);
         } else {
-            yield return StartCoroutine(MakeMove(n - 1, fr, spare, to));
-            yield return StartCoroutine(MakeMove(1, fr, to, spare));
-            yield return StartCoroutine(MakeMove(n - 1, spare, to, fr));
+            yield return StartCoroutine(MakeMove(n - 1, fr, 1, spare, 3, to, 2));
+            yield return StartCoroutine(MakeMove(1, fr, 1, to, 2, spare, 3));
+            yield return StartCoroutine(MakeMove(n - 1, spare, 3, to, 2, fr, 1));
         }
     }
 
