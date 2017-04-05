ab220202061802a47ce884b825c66acbec391cb3
Wahid Shafique <wahidshafique@hotmail.com>
2017-04-05 10:59:09 -0400

    finishing touches

--- Assets/Scripts/DiscTouch.cs
+++ Assets/Scripts/DiscTouch.cs
@@ -13,11 +13,8 @@ public class DiscTouch : MonoBehaviour {
     private Renderer myRenderer;
     private Rigidbody rig;
     [SerializeField]
-    Transform destHanoi1;
-    [SerializeField]
-    Transform destHanoi2;
-    [SerializeField]
-    Transform destHanoi3;
+    Transform[] destHanoiTops;
+    Vector3[] refposes;
 
     //physics
     private float travelTime;
@@ -26,12 +23,6 @@ public class DiscTouch : MonoBehaviour {
     float forceForHeight;
     private Vector3 force = Vector3.zero;
     private Vector3 desiredDisplacement = Vector3.zero;
-
-    //references for posts
-    Vector3 refPos1;
-    Vector3 refPos2;
-    Vector3 refPos3;
-
     Collider[] attachedColliders;
 
     [Tooltip("Layer to ignore for the beam")]
@@ -39,6 +30,9 @@ public class DiscTouch : MonoBehaviour {
     #endregion variables
 
     void Awake() {
+        print("tops len" + destHanoiTops.Length);
+        refposes = new Vector3[destHanoiTops.Length];
+        LoadRefs();
         if (transform.CompareTag("TorusSmall")) {
             valueID = 0;
         } else if (transform.CompareTag("TorusMedium")) {
@@ -53,7 +47,7 @@ public class DiscTouch : MonoBehaviour {
             valueID = 4;
         }
 
-myRenderer = GetComponent<Renderer>();
+        myRenderer = GetComponent<Renderer>();
         rig = GetComponent<Rigidbody>();
         attachedColliders = GetComponents<Collider>();
     }
@@ -61,21 +55,27 @@ myRenderer = GetComponent<Renderer>();
     void Start() {
         isInteractable = true;
         origZ = transform.position.z;
-        refPos1 = GetOffsetPosition(destHanoi1, false);
-        refPos2 = GetOffsetPosition(destHanoi2, false);
-        refPos3 = GetOffsetPosition(destHanoi3, false);
+    }
+
+    void LoadRefs() {
+        for (int i = 0; i < destHanoiTops.Length; i++) {
+            refposes[i] = GetOffsetPosition(destHanoiTops[i], false);
+        }
     }
 
     public void MoveDisc(int destinationNum, float initdelay) {
         switch (destinationNum) {
             case 1:
-                StartCoroutine(WaitAndTravel(1.0f, refPos1, initdelay));
+                StartCoroutine(WaitAndTravel(1.0f, refposes[0], initdelay));
                 break;
             case 2:
-                StartCoroutine(WaitAndTravel(1.0f, refPos2, initdelay));
+                StartCoroutine(WaitAndTravel(1.0f, refposes[1], initdelay));
                 break;
             case 3:
-                StartCoroutine(WaitAndTravel(1.0f, refPos3, initdelay));
+                StartCoroutine(WaitAndTravel(1.0f, refposes[2], initdelay));
+                break;
+            case 4:
+                StartCoroutine(WaitAndTravel(1.0f, refposes[3], initdelay));
                 break;
             default:
                 print("You suppied bad argument");
@@ -88,9 +88,7 @@ myRenderer = GetComponent<Renderer>();
         LaunchProjectile();
         yield return new WaitForSeconds(waitTime);
         rig.velocity = Vector3.zero;
-        refPos1 = GetOffsetPosition(destHanoi1, false);
-        refPos2 = GetOffsetPosition(destHanoi2, false);
-        refPos3 = GetOffsetPosition(destHanoi3, false);
+        LoadRefs();
         Vector3 ourPos = GetOffsetPosition(this.transform, true);
         desiredDisplacement = refPos - ourPos;
         LaunchProjectileToDest();
--- Assets/Scripts/HanoiScript.cs
+++ Assets/Scripts/HanoiScript.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HanoiScript : MonoBehaviour {
+    Text winner;
     Button autoSolveBtn;
     [SerializeField]
     DiscTouch[] discs;
@@ -19,71 +20,92 @@ public class HanoiScript : MonoBehaviour {
     private bool autoRunReset = false;
     private bool canInteract = true;
     private bool isNotMakingMove = true;
-
-    void Awake() {
-        //discs = new DiscTouch[4];
-    }
+    private bool gameOver = false;
     void Start() {
         print("d len" + discs.Length);
         selectedOtherPoleval = 999;
         selectedRealPole = null;
         selectedOtherPole = null;
+        winner = GameObject.Find("Winner").GetComponent<Text>();
+
         autoSolveBtn = GameObject.Find("AutoSolve").GetComponent<Button>();
         //create the poles for reference
         poles = new Stack<DiscTouch>[hanoiTowers.Length];
 
         //init stack inside of each pole
-        poles[0] = new Stack<DiscTouch>();
-        poles[1] = new Stack<DiscTouch>();
-        poles[2] = new Stack<DiscTouch>();
-        clearAndResetAllPoles(3);
+        for (int i = 0; i < poles.Length; i++) {
+            poles[i] = new Stack<DiscTouch>();
+        }
+        clearAndResetAllPoles();
     }
 
     void Update() {
-        if (Input.GetButtonDown("Fire1")) {
-            RaycastHit hitinfo;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hitinfo, 100, 1 << 8)) {
-                print(hitinfo.transform.gameObject.name);
-                if (isNotMakingMove) {
-                    //hitinfo.transform.GetComponentInChildren<GlowObject>().ExitGlow();
-                    if (hitinfo.transform.CompareTag("Hanoi1") && poles[0].Count > 0) {
-                        hitinfo.transform.GetComponentInChildren<GlowObject>().TriggerGlow();
-                        selectedRealPole = hitinfo.transform.gameObject;
-                        StartCoroutine(makeSubsequentMove(selectedRealPole, poles[0]));
-                    } else if (hitinfo.transform.CompareTag("Hanoi2") && poles[1].Count > 0) {
-                        hitinfo.transform.GetComponentInChildren<GlowObject>().TriggerGlow();
-                        selectedRealPole = hitinfo.transform.gameObject;
-                        StartCoroutine(makeSubsequentMove(selectedRealPole, poles[1]));
-                    } else if (hitinfo.transform.CompareTag("Hanoi3") && poles[2].Count > 0) {
-                        hitinfo.transform.GetComponentInChildren<GlowObject>().TriggerGlow();
-                        selectedRealPole = hitinfo.transform.gameObject;
-                        StartCoroutine(makeSubsequentMove(selectedRealPole, poles[2]));
+        if (!gameOver) {
+            if (Input.GetButtonDown("Fire1")) {
+                RaycastHit hitinfo;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hitinfo, 100, 1 << 8)) {
+                    print(hitinfo.transform.gameObject.name);
+                    if (isNotMakingMove) {
+                        if (hitinfo.transform.CompareTag("Hanoi1") && poles[0].Count > 0) {
+                            hitinfo.transform.GetComponentInChildren<GlowObject>().TriggerGlow();
+                            selectedRealPole = hitinfo.transform.gameObject;
+                            StartCoroutine(makeSubsequentMove(selectedRealPole, poles[0]));
+                        } else if (hitinfo.transform.CompareTag("Hanoi2") && poles[1].Count > 0) {
+                            hitinfo.transform.GetComponentInChildren<GlowObject>().TriggerGlow();
+                            selectedRealPole = hitinfo.transform.gameObject;
+                            StartCoroutine(makeSubsequentMove(selectedRealPole, poles[1]));
+                        } else if (hitinfo.transform.CompareTag("Hanoi3") && poles[2].Count > 0) {
+                            hitinfo.transform.GetComponentInChildren<GlowObject>().TriggerGlow();
+                            selectedRealPole = hitinfo.transform.gameObject;
+                            StartCoroutine(makeSubsequentMove(selectedRealPole, poles[2]));
+                        } else if (hitinfo.transform.CompareTag("Hanoi4") && poles[3].Count > 0) {
+                            hitinfo.transform.GetComponentInChildren<GlowObject>().TriggerGlow();
+                            selectedRealPole = hitinfo.transform.gameObject;
+                            StartCoroutine(makeSubsequentMove(selectedRealPole, poles[3]));
+                        } else {
+                            print("Invalid Move");
+                        }
                     } else {
-                        print("Invalid Move");
+                        if (hitinfo.transform.CompareTag("Hanoi1")) {
+                            selectedOtherPoleval = 0;
+                            selectedOtherPole = hitinfo.transform.gameObject;
+                            //StartCoroutine(makeSubsequentMove(selectedRealPole, poles[0]));
+                        } else if (hitinfo.transform.CompareTag("Hanoi2")) {
+                            selectedOtherPoleval = 1;
+                            selectedOtherPole = hitinfo.transform.gameObject;
+                        } else if (hitinfo.transform.CompareTag("Hanoi3")) {
+                            selectedOtherPoleval = 2;
+                            selectedOtherPole = hitinfo.transform.gameObject;
+                        } else if (hitinfo.transform.CompareTag("Hanoi4")) {
+                            selectedOtherPoleval = 3;
+                            selectedOtherPole = hitinfo.transform.gameObject;
+                        } else {
+                            isNotMakingMove = true;
+                            //selectedRealPole.GetComponent<GlowObject>().ExitGlow();
+                        }
+                        //first check to see if the stack even has something on top
+                        //if it does then wait for player to make next move
+                        //else return 
+                        //now wait to select next move
                     }
-                } else {
-                    if (hitinfo.transform.CompareTag("Hanoi1")) {
-                        selectedOtherPoleval = 0;
-                        selectedOtherPole = hitinfo.transform.gameObject;
-                        //StartCoroutine(makeSubsequentMove(selectedRealPole, poles[0]));
-                    } else if (hitinfo.transform.CompareTag("Hanoi2")) {
-                        selectedOtherPoleval = 1;
-                        selectedOtherPole = hitinfo.transform.gameObject;
-                    } else if (hitinfo.transform.CompareTag("Hanoi3")) {
-                        selectedOtherPoleval = 2;
-                        selectedOtherPole = hitinfo.transform.gameObject;
-                    } else {
-                        isNotMakingMove = true;
-                        //selectedRealPole.GetComponent<GlowObject>().ExitGlow();
-                    }
-                    //first check to see if the stack even has something on top
-                    //if it does then wait for player to make next move
-                    //else return 
-                    //now wait to select next move
-
-                    //isNotMakingMove = false;
                 }
+            }
+        } else {
+            winner.text = "Winner!";
+        }
+    }
+    void checkState() {
+        //for (int i = 0; i < discs.Length; i++) {
+        //    if (poles[1].Contains(discs[i])) {
+        //        tempChState = true;
+        //    } else {
+        //        break;
+        //    }
+        //}
+        for (int i = 0; i < poles.Length; i++) {
+            if (poles[i].Count >= discs.Length) {
+                gameOver = true;
             }
         }
     }
@@ -119,14 +141,13 @@ public class HanoiScript : MonoBehaviour {
             if (autoSolveBtn.interactable) {
                 autoSolveBtn.interactable = false;
             }
-
+            //done move
+            checkState();
         } else {
             sel.transform.GetComponentInChildren<GlowObject>().TriggerGlow(Color.red);
             print("You cannot put larger piece to the top of a smaller one");
         }
         //both cases are assumed to be true, launch the from -> to 
-        //DiscTouch tempFrom = temp
-        //DiscTouch tempFrom = tempSelection
     }
 
     public void callSolve() {
@@ -134,7 +155,7 @@ public class HanoiScript : MonoBehaviour {
             autoSolveBtn.interactable = false;
             StartCoroutine(SolveAllTasker());
         } else {
-            clearAndResetAllPoles(3);
+            clearAndResetAllPoles();
         }
     }
 
@@ -159,7 +180,7 @@ public class HanoiScript : MonoBehaviour {
         autoRunReset = true;
     }
 
-    void clearAndResetAllPoles(int numDiscs) {
+    void clearAndResetAllPoles() {
         foreach (Stack<DiscTouch> pole in poles) {
             pole.Clear();
         }
@@ -167,8 +188,6 @@ public class HanoiScript : MonoBehaviour {
         for (int i = discs.Length - 1; i >= 0; i--) {
             discs[i].MoveDisc(1, increma);
             increma += 0.5f;
-            //discs[1].MoveDisc(1, 1.0f);
-            //discs[0].MoveDisc(1, 1.5f);
         }
         autoRunReset = false;
         for (int i = discs.Length - 1; i >= 0; i--) {
